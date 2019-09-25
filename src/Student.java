d788d685acb28c10cf028f97b190a7d454d69520
Lam Duong <lamdu@Lams-MacBook-Pro.local>
2019-09-25 14:45:43 +0300

    first commit

---
+++ src/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    private int id;
+    private String name;
+    private  int grade;
+    private int feePaid;
+    private int feeTotal;
+
+    public Student(int id, String name, int grade, int feeTotal) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+        this.feePaid = 0;
+        this.feeTotal = 30000;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+    public void updateFeePaid(int fees) {
+        feePaid += fees;
+    }
+}
